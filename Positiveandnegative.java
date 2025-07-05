37937b30f0d34dc0b095ed88ac1b23c5bb6baaae
vignesh26061 <165666723+vignesh26061@users.noreply.github.com>
2025-07-05 18:51:11 +0530

    Add files via upload

---
+++ Positiveandnegative.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class import java.util.*;
+class Positiveandnegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        if(a>0)
+        {
+            System.out.println("positive number");
+        }
+        else if(a<0)
+        {
+            System.out.println("Negative number");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+    }
