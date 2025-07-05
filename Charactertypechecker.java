31b667a5164a16f278ac85a4655c4057d1bf539a
vignesh26061 <165666723+vignesh26061@users.noreply.github.com>
2025-07-05 18:51:59 +0530

    Add files via upload

---
+++ Charactertypechecker.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Charactertypechecker {
+    public static void main(String[] args) {
+        char ch = '.';
+        if (Character.isLetter(ch)) {
+            System.out.println("Alphabet"); 
+        } else if (Character.isDigit(ch)) {
+            System.out.println("Digit"); 
+        } else {
+            System.out.println("Special Character"); 
+        }
+    }
+}
