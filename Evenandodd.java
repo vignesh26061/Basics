28fe7b5bed926d6ac17d78df7def81f3bc68a0d4
vignesh26061 <165666723+vignesh26061@users.noreply.github.com>
2025-07-05 18:51:40 +0530

    Add files via upload

---
+++ Evenandodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Evenandodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        if(a%2==0)
+        {
+            System.out.println("Even number");
+        }
+        else 
+        {
+            System.out.println("odd  number");
+        }
+       
+    }
+}
